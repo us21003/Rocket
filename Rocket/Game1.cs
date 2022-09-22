09e856a61a20b552c4e703e436a428ee64fec2c6
alexu503 <us21003@ues.edu.sv>
2022-09-21 21:04:39 -0600

    Documenting code

--- Rocket/Game1.cs
+++ Rocket/Game1.cs
@@ -23,21 +23,21 @@ namespace Rocket {
         Rectangle rocketPosition;
         Rectangle diamondPosition;
 
-        int spriteXRocket;
-        int spriteYRocket;
-        int moveXRocket;
-        int moveYRocket;
-        int numberOfSpaceRocks = 15;
-        int spriteYComet = 0;
-        int spriteXComet = 900;
-        int diamondX;
-        int diamondY;
+        int spriteXRocket; //Posicion en el eje x del cometa
+        int spriteYRocket; //Posicion en el eje y del cometa
+        int moveXRocket; //Movimiento en x del cometa
+        int moveYRocket; //Movimiento en y del cometa
+        int numberOfSpaceRocks = 15; //Cantidad de obstaculos a agregar
+        int spriteYComet = 0; //Posicion en el eje y donde apareceran
+        int spriteXComet = 900; //Posicion en el eje x donde apareceran
+        int diamondX; //Posicion en x del diamante
+        int diamondY; //Lo mismo, pero en y
         int spriteRocketAddress = 2;//0=Arriba,1=Abajo,2=Izquierda,3=Derecha
-        int points = 0;
-        int collisionX = 0;
-        int collisionY = 0;
-        int[] spaceRockX;
-        int[] spaceRockY;
+        int points = 0; //Puntos por cada diamante capturado
+        int collisionX = 0; //Posicion en x donde hubo colision
+        int collisionY = 0; //Lo mismo, pero en y
+        int[] spaceRockX; //Arreglo de posiciones de cada cometa en el eje x
+        int[] spaceRockY; //Lo mismo, pero en y
 
         bool hasCrashed = false;
 
@@ -57,6 +57,7 @@ namespace Rocket {
 
         #region LoadDyamond
 
+        //Indicar en donde hacer aparecer el diamante, usando un metodo aleatorio
         private void LoadDiamond() {
             diamondX = r.Next(0, 900);
             diamondY = r.Next(0, 625);
@@ -67,7 +68,7 @@ namespace Rocket {
         #endregion
 
         #region CreateRectangles
-
+        //Crear rectangulos a aprtir de las posiciones de los cometas y sus dimensiones
         private void CreateRectangles(int positionX, int positionY) {
             cometPosition = new Rectangle[numberOfSpaceRocks];
             
@@ -80,8 +81,8 @@ namespace Rocket {
         #endregion
 
         #region CreateRocks
+        //Dar la posicion y el numero de cometas
         private void CreateRocks() {
-            //rockMovesY = new int[numberOfSpaceRocks];
             spaceRockX = new int[numberOfSpaceRocks];
             spaceRockY = new int[numberOfSpaceRocks];
 
@@ -89,26 +90,30 @@ namespace Rocket {
                 spaceRockX[i] = r.Next(800, 900);
                 spaceRockY[i] = r.Next(0, 625);
 
+                //LLamamos a la funcion para agregarles un margen de rectangulo
                 CreateRectangles(spaceRockX[i], spaceRockY[i]);
             }
         }
         #endregion
 
         #region hasCrashed
-
+        //funcion que comprueba si el cohete ha chocado contra un cometa
         private void RocketHasCrashed(Rectangle[] cometPosition, int positionX, int positionY) {
             for(int x = 0; x < numberOfSpaceRocks; x++) {
                 if (rocketPosition.Intersects(cometPosition[x])) {
                     hasCrashed = true;
+                    //guardamos las posiciones donde ha colisionado, para, posteriormente, colocar una textura de colision ahi
                     collisionX = positionX;
                     collisionY = positionY;
                 }
             }
         }
 
+        //Comprobamos si se ha capturado al diamante (interceptado)
         private void RocketHasTheDiamond(Rectangle rocketPosition) {
             if (rocketPosition.Intersects(diamondPosition)) {
                 points++;
+                //Si se ha capturado, hacemos aparecer uno nuevo en una nueva posicion aleatoria diferente
                 this.LoadDiamond();
             }
         }
@@ -116,7 +121,7 @@ namespace Rocket {
         #endregion
 
         #region moveComets
-
+        //Mover los cometas; los hacemos aparecer en una posicin aleatoria en Y
         private void MoveComets() {
             spriteYComet = r.Next(0, 615);
         }
@@ -136,6 +141,7 @@ namespace Rocket {
         #region LoadContent
         protected override void LoadContent() {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            //Cargando texturas...
             myRocketUp = Content.Load<Texture2D>("rocket0");
             myRocketDown = Content.Load<Texture2D>("rocket1");
             myRocketLeft = Content.Load<Texture2D>("rocket2");
@@ -157,10 +163,11 @@ namespace Rocket {
             KeyboardState keyboardState = Keyboard.GetState();
             Keys[] pressedKey = keyboardState.GetPressedKeys();
 
+            //Hacer que los cometas se muevan cada segundo en -50
             if (gameTime.TotalGameTime.Milliseconds == 0) {
                 spriteXComet-=50;
 
-                //Movimiento infinito: una vez fuera de la pantalla, vuelven a aparecer.
+                //"Movimiento infinito": una vez fuera de la pantalla, vuelven a aparecer en el inicio, pero en posiciones diferentes (aleatorias).
                 if(spriteXComet < 0) {
                     spriteXComet = 900;
                     for(int x = 0; x < numberOfSpaceRocks; x++) {
@@ -170,12 +177,14 @@ namespace Rocket {
                 }
             }
 
+            //Captura de eventos de teclado
             foreach (Keys key in pressedKey) {
                 if (key == Keys.Up) {
                     moveYRocket = -5;
                     moveXRocket = 0;
 
                     spriteYRocket = spriteYRocket + moveYRocket;
+                    //Capturamos la direccino tomada para cambiar la textura luego
                     spriteRocketAddress = 0;
                 }
 
@@ -208,6 +217,7 @@ namespace Rocket {
                 }
             }
 
+            //Asignar limites a los margenes
             if(spriteXRocket < 0) {
                 spriteXRocket = 0;
             }
@@ -216,7 +226,7 @@ namespace Rocket {
                 spriteYRocket = 0;
             }
 
-            //Prevent to go aoutside the screen
+            //Asignar limites al limite: evitamos que llegue al limite completo para que no se vea bugueado
             if (spriteXRocket + moveXRocket > 860) {
                 spriteXRocket = 860;
             }
@@ -237,11 +247,15 @@ namespace Rocket {
             _spriteBatch.Draw(mySpace, new Rectangle(0,0,950,650), Color.White);
             _spriteBatch.Draw(myDiamond, new Rectangle(diamondX, diamondY, 50, 50), Color.White);
 
+            //Movemos los cometas y hacemos aparecerlos: iniciaran en una posicion definida aleatoriamente menos la posicion inicial del cometa,
+            //para que luzca que se mueve desde el vacio del espacio (?
             for (int i = 0; i < numberOfSpaceRocks; i++) {
                 _spriteBatch.Draw(mySpacialRock, new Rectangle(spaceRockX[i] - spriteXComet, spaceRockY[i], 70, 40), Color.White);
             }
 
+            //Arreglo de posiciones: hacemos cargar todas las rocas en posiciones diferentes y que se muevan constantemente 
             for(int x = 0; x < numberOfSpaceRocks; x++) {
+                //Dependiendo del la direccion, cargamos una textura diferente
                 switch (spriteRocketAddress) {
                     case 0:
                         _spriteBatch.Draw(myRocketUp, new Rectangle(900 - spriteXRocket, spriteYRocket, 40, 70), Color.White);
@@ -260,16 +274,17 @@ namespace Rocket {
                         rocketPosition = new Rectangle(spriteXRocket, 325 + spriteYRocket, myRocketUp.Width, myRocketUp.Height);
                         break;
                 }
+                //Obtenemos la posicion del diamante y vamos comparando con la del cometa (working with some bugs)
                 this.RocketHasTheDiamond(rocketPosition);
 
-                if (hasCrashed) {
-                    System.Threading.Thread.Sleep(5000);
+                //Comprobamos si no ha colisionado (not working).
+                if (hasCrashed) {                    
                     _spriteBatch.Draw(collision, new Rectangle(collisionX, collisionY, 100, 100), Color.White);
                     _spriteBatch.DrawString(myFont, "Game over\nPoints earned: " + points, new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2) ,Color.White);
-                    this.Exit();
+                    System.Threading.Thread.Sleep(5000);
+                    this.Exit(); //Aca iba a ir alguna animacion, pero al no cargar el evento de colision, no puse na'a
                 }
             }
-
             _spriteBatch.End();
             base.Draw(gameTime);
         }
